8978cefd81df4be6d2e032c915b8800cd8507abb
K.Kimura <dotnsf@gmail.com>
2022-12-11 11:09:31 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# GPT 3.5 based query Node for Node-RED
+
+
+## Overview
+
+GPT 3.5 ベースの問い合わせノード
+
+
+## Licensing
+
+This code is licensed under MIT.
+
+
+## Copyright
+
+2022 K.Kimura @ Juge.Me all rights reserved.
+
---
+++ node.js
@@ -0,0 +1,50 @@
+var axios = require( 'axios' );
+
+module.exports = function( RED ){
+  function main( config ){
+    RED.nodes.createNode( this, config );
+    var node = this;
+    node.on( 'input', function( msg ){
+      var text = msg.payload;
+      var apikey = this.apikey;
+      if( apikey ){
+        if( text ){
+          axios.post( 'https://api.openai.com/v1/completions', {
+            prompt: text,
+            model: 'text-davinci-003',
+            max_tokens: 4000
+          }, {
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer ' + apikey 
+            }
+          }).then( function( result ){
+            if( result.data && result.data.choices && result.data.choices.length > 0 ){
+              var answer = result.data.choices[0].text;
+
+              //. 最初の "\n\n" 以降が正しい回答？
+              var tmp = answer.split( "\n\n" );
+              if( tmp.length > 1 && tmp[0].length < IGNORE_PHRASE ){
+                tmp.shift();
+                answer = tmp.join( "\n\n" );
+              }
+              msg.payload = answer;
+              node.send( msg );
+            }else{
+              msg.payload = JSON.stringify( result );
+              node.send( msg );
+            }
+          });
+        }else{
+          msg.payload = 'Query text is missing.';
+          node.send( msg );
+        }
+      }else{
+        msg.payload = 'API Key is missing.';
+        node.send( msg );
+      }
+    });
+  };
+
+  RED.nodes.registerType( 'GPT3.5', main );
+}
